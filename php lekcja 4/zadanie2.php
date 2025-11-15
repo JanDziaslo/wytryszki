3cdb5687c2fbcd245c4971386656431fbf142f38
jandziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2025-11-15 15:23:15 +0100

    zadanie2 dokonczone

--- php lekcja 4/zadanie2.php
+++ php lekcja 4/zadanie2.php
@@ -6,24 +6,27 @@
     <title>zadanie2</title>
 </head>
 <body>
-<h1>Zakup zeszytów</h1>
-<form action="zadanie1.php" method="post">
-    <p><b> Kwota w zl</b>
+<h1>Przelicznik walut</h1>
+<form action="zadanie2.php" method="post">
+    <p><b>Kwota w zl</b>
     <br><input type="text" name="kwota"></p>
-    <p><input type="radio" value="Funt"></p>
-    <p><input type="radio" value="Euro"></p>
+    <p><input type="radio" name="waluta" value="Funt"> Funt brytyjski</p>
+    <p><input type="radio" name="waluta" value="Euro"> Euro</p>
     <p><input type="submit" value="Przelicz"></p>
 </form>
 <?php
-echo "<p> Dokonales nastepujacych zakupow: </p>";
-$linie=$_POST['linie'];
-$kratke=$_POST['kratke'];
-$gladkie=$_POST['gladkie'];
-echo "<p> Zeszyty w linie: $linie szt. </p>";
-echo "<p> Zeszyty w kratke: $kratke szt. </p>";
-echo "<p> Zeszyty gladkie: $gladkie szt. </p>";
-$koszt_wszytkie=($linie*1.5)+($kratke*1.3)+($gladkie*1);
-echo "<p> Koszt wszystkich zeszytow wynosi: $koszt_wszytkie zl </p>";
+if(isset($_POST['kwota']) && isset($_POST['waluta'])){
+    $kwota = floatval($_POST['kwota']);
+    $waluta = $_POST['waluta'];
+
+    if($waluta == "Funt"){
+        $wynik = floatval($kwota / 5.2);
+        echo "<p>Kwota $kwota zł to " . round($wynik, 2) . " funtów brytyjskich</p>";
+    } elseif($waluta == "Euro"){
+        $wynik = floatval($kwota / 4.3);
+        echo "<p>Kwota $kwota zł to " . round($wynik, 2) . " euro</p>";
+    }
+}
 ?>
 </body>
 </html>
