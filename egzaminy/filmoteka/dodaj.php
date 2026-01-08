263dae62813abc115068cdf8c9d393ac625d715c
JanDziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2026-01-08 12:53:51 +0100

    poprawiony php - oczywiscie ze tabela nadaje sie na smieci ale niech bedzie ze dziala

--- egzaminy/filmoteka/dodaj.php
+++ egzaminy/filmoteka/dodaj.php
@@ -1,18 +1,18 @@
 <?php
-$tytul = $POST['tytul'];
-$gatunek = $POST['gatunek'];
-$rok = $POST['rok'];
-$ocena = $POST['ocena'];
-
-
 $serwer = "127.0.0.1:13306";
 $uzyt = "wytrychy_user";
 $haslo = "gDxajVS2BhMiqcY8xWHU34EpjRpC489T";
 $baza = "wytrychy_db";
 
-$p = mysqli_connect("$serwer", "$uzyt", "$haslo", "$baza") or die("Problem z serwerem!");
+$p = mysqli_connect($serwer, $uzyt, $haslo, $baza) or die("Problem z serwerem!");
 mysqli_set_charset($p, "utf8");
-$zapytanie = "INSERT INTO filmy (tytul, gatunki_id, rok, ocena) VALUES ('$tytul', '$gatunek', '$rok', '$ocena')";
+
+$tytul = $_POST['tytul'];
+$gatunek = $_POST['gatunek'];
+$rok = $_POST['rok'];
+$ocena = $_POST['ocena'];
+
+$zapytanie = "INSERT INTO filmy (tytul, gatunki_id, rok, ocena, rezyserzy_id) VALUES ('$tytul', '$gatunek', '$rok', '$ocena', '1')";
 mysqli_query($p, $zapytanie);
 mysqli_close($p);
 echo "Film $tytul został dodany do bazy";
